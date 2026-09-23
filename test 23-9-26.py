65cad3a73a2be27e9982a547cc010df920897979
Senthil Kanagavel Balasundaram <167542767+senthil-kanagavel-05@users.noreply.github.com>
2026-09-23 11:21:55 +0530

    Update test 23-9-26.py

--- test 23-9-26.py
+++ test 23-9-26.py
@@ -1,1 +1,30 @@
+## BINARY PROGRAM
+numbers = input().split(",")
+result = []
 
+for i in numbers:
+    num = int(i,2)
+    if num%5==0:
+        result.append(i)
+print(",".join(result))
+
+## LETTER AND DIGIT PROGRAM
+
+sentence = input()
+
+l = 0
+d = 0
+for i in sentence:
+    if i.isalpha():
+        l+=1
+    elif i.isdigit():
+        d+=1
+print("Letters: ",l)
+print("Digit: ",d)
+
+## FACTORIAL PROGRAM
+n = int(input())
+fact = 1
+for i in range(1,n+1):
+    fact = fact*i
+print("Factorial: ",fact)
